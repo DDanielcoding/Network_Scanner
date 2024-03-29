483f036ac8c90f25dff002d941d40e4b94b83e72
DDanielCoding <draskoczi.daniel@gmail.com>
2024-03-29 08:28:15 -0400

    Update: Code improvement, using a list of dictionaries

--- network_scanner.py
+++ network_scanner.py
@@ -9,8 +9,11 @@ def scan(ip):
     answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
     print("IP\t\t\tMAC Address\n---------------------------------------------")
+    clients_list = []
     for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(client_dict)
         print(element[1].psrc + "\t\t" + element[1].hwsrc)
-
+    print(clients_list)
 
 scan("172.22.82.135/24")
